345402ecc0d1fa2fa897560b960139192fe22037
SoulCodingYanhun <155857563+SoulCodingYanhun@users.noreply.github.com>
2024-05-19 08:12:31 +0800

    Update time.py

--- salgorithm/time.py
+++ salgorithm/time.py
@@ -1,12 +1,15 @@
-
 import time
 
-def timeit(func):
-    """装饰器，用于计算函数运行时间"""
-    def wrapper(*args, **kwargs):
+class TimedFunction:
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args, **kwargs):
         start_time = time.time()  # 开始时间
-        result = func(*args, **kwargs)  # 执行函数
+        result = self.func(*args, **kwargs)  # 执行函数
         end_time = time.time()  # 结束时间
-        print(f"{func.__name__} took {:.6f} seconds.".format(end_time - start_time))
+        print(f"{self.func.__name__} took {:.6f} seconds.".format(end_time - start_time))
         return result
-    return wrapper
+
+    def time(self, *args, **kwargs):
+        return self(*args, **kwargs)
