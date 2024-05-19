9ef88a097b2cbd4d482a1cac99fe759b2f7723c5
SoulCodingYanhun <zhuzhishengzhu6@outlook.com>
2024-05-19 01:03:48 +0000

    main

--- salgorithm/time.py
+++ salgorithm/time.py
@@ -8,7 +8,7 @@ class TimedFunction:
         start_time = time.time()  # 开始时间
         result = self.func(*args, **kwargs)  # 执行函数
         end_time = time.time()  # 结束时间
-        print(f"{self.func.__name__} took {:.6f} seconds.".format(end_time - start_time))
+        print(f"{self.func.__name__} took {end_time - start_time:.6f} seconds.")
         return result
 
     def time(self, *args, **kwargs):
